f80e12ed79545c4a6757ae1f2c00afecf285c73d
EFLLP <erickllanos30@gmail.com>
2018-12-27 23:09:46 -0600

    Inicio de aplicaciónes en Xamarin  :smile:
    Inicio de practicas en Xamarin.

---
+++ ColorSwitcher/ColorSwitcher/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ColorSwitcher
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void ChangeColor(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            var red = redSlider.Value;
+            var blue = blueSlider.Value;
+            var green = greenSlider.Value;
+
+            Color bgColor =new Color(red, green, blue);
+            boxColor.BackgroundColor = bgColor;
+            lblDisplay.Text = ColorToHext(bgColor);
+        }
+
+        private string ColorToHext(Color bgColor)
+        {
+            int red = (int)(bgColor.R * 255);
+            int green = (int)(bgColor.G * 255);
+            int blue = (int)(bgColor.B * 255);
+            int alpha = (int)(bgColor.A * 255);
+            return $"#{red:X2}{green:X2}{blue:X2}{alpha:X2}";
+        }
+    }
+}
---
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void btnCalcular_Clicked(object sender, System.EventArgs e)
+        {
+            var total = (decimal.Parse(txtTotal.Text));
+            var propina = (int.Parse(txtPropina.Text));
+            var noPersona = (int.Parse(txtNumPersonas.Text));
+            var totalPropina = ((total * propina) / 100);
+            lblPropina.Detail = totalPropina.ToString("C");
+            lblTotal.Detail = (total + totalPropina).ToString("C");
+            lblPropinaPorPersona.Detail = (totalPropina / noPersona).ToString("C");
+            lblTotalPorPersona.Detail = ((total + totalPropina) / noPersona).ToString("C");
+        }
+    }
+}
